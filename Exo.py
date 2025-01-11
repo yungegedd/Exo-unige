ed148ab01b49b1749b8ddc262df79dc44cb78380
Glory DANIGUE <glorydanigue423@gmail.com>
2025-01-11 01:26:00 +0100

    #########################

--- Exo.py
+++ Exo.py
@@ -1,3 +1,11 @@
 nom =input("Entrez votre nom : ")
 prenom =input("Entrez votre prenom : ")
 print("Bonjour",prenom,nom)
+a="le chat"
+b= "chasse"
+c= "la souris"
+
+s=a+" "+b
+print(s)
+s+=" "+c
+print(s)
