7aec16dfb0323e8509479261f32597663121ebca
Glory DANIGUE <glorydanigue423@gmail.com>
2025-02-23 13:23:41 +0100

    New change

--- Exopython.py
+++ Exopython.py
@@ -1,42 +1,47 @@
-# Exercice 1 : Indentation et Boucles
-# Écrire un programme qui affiche les nombres de 1 à 10.
-#  Pour chaque nombre, afficher "pair" s'il est pair et "impair" sinon.
+import random
 
-for i in range(1,11):
-    if i%2==0:
-        print("i=",i ,"est pair")
+from utils import exercice , formatter_sudoku ,generer_personnes,generer_resultats, obtenir_sudoku_valide
+
+def ExerxciceIA():
+    # Exercice 1 : Indentation et Boucles
+    # Écrire un programme qui affiche les nombres de 1 à 10.
+    #  Pour chaque nombre, afficher "pair" s'il est pair et "impair" sinon.
+
+    for i in range(1,11):
+        if i%2==0:
+            print("i=",i ,"est pair")
+        else:
+            print("i=",i ,"est impair")    
+
+    # Exercice 2 : Utilisation des types de base
+    # Écrire un programme qui demande à
+    #  l'utilisateur de saisir un nombre entier.
+    #   Le programme doit ensuite afficher ce nombre, 
+    #   son double et son carré.      
+
+    a = int(input("Entrez un nombre entier : "))
+    print(f"le nombre est {a} et son double est {2*a}, son carré est {a*a}") 
+
+    #Exercice 3 : Opérateurs et contrôle du flot
+    # Écrire un programme 
+    # qui prend une chaîne de caractères en entrée et vérifie
+    #  si elle contient au moins un caractère numérique.
+
+    chaine = input("Entrez une chaine de caractères : ")
+    for i in chaine:
+        if i.isdigit():
+            print("La chaine contient un caractère numérique")
+            break
+        else:
+            print("La chaine ne contient pas de caractère numérique")
+            break
+
+    chaine = input("Saisissez une chaîne de caractères: ")
+    contient_chiffre = any(char.isdigit() for char in chaine)
+    if contient_chiffre:
+        print("La chaîne contient au moins un chiffre.")
     else:
-        print("i=",i ,"est impair")    
-
-# Exercice 2 : Utilisation des types de base
-# Écrire un programme qui demande à
-#  l'utilisateur de saisir un nombre entier.
-#   Le programme doit ensuite afficher ce nombre, 
-#   son double et son carré.      
-
-a = int(input("Entrez un nombre entier : "))
-print(f"le nombre est {a} et son double est {2*a}, son carré est {a*a}") 
-
-#Exercice 3 : Opérateurs et contrôle du flot
-# Écrire un programme 
-# qui prend une chaîne de caractères en entrée et vérifie
-#  si elle contient au moins un caractère numérique.
-
-chaine = input("Entrez une chaine de caractères : ")
-for i in chaine:
-    if i.isdigit():
-        print("La chaine contient un caractère numérique")
-        break
-    else:
-        print("La chaine ne contient pas de caractère numérique")
-        break
-
-chaine = input("Saisissez une chaîne de caractères: ")
-contient_chiffre = any(char.isdigit() for char in chaine)
-if contient_chiffre:
-    print("La chaîne contient au moins un chiffre.")
-else:
-    print("La chaîne ne contient aucun chiffre.")
+        print("La chaîne ne contient aucun chiffre.")
 
 #Exercice 4 : 
 # Écrire un programme qui prend le prénom et le nom 
@@ -45,8 +50,14 @@ else:
 
 Nom = input("Entrez votre nom : ")
 Prenom = input("Entrez votre prénom : ")
+Date de naissance = input("Entrez votre date de naissance : ")
 print(f"Bienvenue {Prenom} {Nom} !")
 
 
 
 
+ExerxciceIA()
+exercice1()
+exercice2()
+exercice3()
+exercice4()
