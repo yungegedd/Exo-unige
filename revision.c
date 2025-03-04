1e79ed11b5a7351c9ffc75eeee137e5aba05625f
Glory DANIGUE <glorydanigue423@gmail.com>
2025-03-04 22:00:49 +0100

    New

--- revision.c
+++ revision.c
@@ -6,6 +6,31 @@
 // syntaxe de base dans C
 
 int main(void) {
-    printf("Hello World\n");
-    return 0;
+//     printf("Hello World\n");
+//     return 0;
+
+// 
+//variables et affectations
+
+// int a_le_nbre_entier;
+// a_le_nbre_entier = 5;
+// printf("a_le_nbre_entier = %d\n", a_le_nbre_entier);
+// return 0;
+
+// int a,b;
+// a=10;
+// b=5;
+// printf("a=%d, b=%d\n", a,b);
+// b=21;
+// printf("a=%d, b=%d\n", a,b);
+// a=11;
+// printf("a=%d, b=%d\n", a,b);
+// return 0;
+
+int a =1000000000;
+printf("a=%d\n", a);
+a += a;
+printf("a=%d\n", a);
+a += a;
+printf("a=%d\n", a);
 }
